d8e6be85a1bfb050674adc4677067775b009f0d4
Martin Ouellet <moa21@sfu.ca>
2016-07-08 14:20:21 -0700

    lecture 120

--- app/src/main/java/com/mgodevelopment/friends/MainActivity.java
+++ app/src/main/java/com/mgodevelopment/friends/MainActivity.java
@@ -1,25 +1,38 @@
 package com.mgodevelopment.friends;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        super.onCreate(savedInstanceState);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (fragmentManager.findFragmentById(android.R.id.content) == null) {
+
+            FriendsListFragment friendsListFragment = new FriendsListFragment();
+            fragmentManager.beginTransaction().add(android.R.id.content, friendsListFragment).commit();
+
+        }
+
+        setContentView(R.layout.activity_main);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -27,7 +40,23 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.addRecord:
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.deleteDatabase:
+                FriendsDialog dialog = new FriendsDialog();
+                Bundle args = new Bundle();
+                args.putString(FriendsDialog.DIALOG_TYPE, FriendsDialog.DELETE_DATABASE);
+                dialog.setArguments(args);
+                dialog.show(getSupportFragmentManager(), "delete_database");
+                break;
+            case R.id.searchRecords:
+                Intent searchIntent = new Intent(MainActivity.this, SearchActivity.class):
+                startActivity(searchIntent);
+                break;
+        }
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
